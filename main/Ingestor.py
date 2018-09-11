7cfab628d7a1288457ccc545aeedc3cbd9b52b64
Jsalaz <jorgesalazar.cs@gmail.com>
2018-09-10 23:44:20 -0400

    Cleaning up and organizing

--- main/Ingestor.py
+++ main/Ingestor.py
@@ -4,114 +4,105 @@ import pprint
 import os
 import fnmatch as fnm
 import ftp_download
-
-
 #from nt import listdir
 
-#filename="Ingest_List_3.json"
-#directory="C:/Users/j_sal/git/Ingest"
-#data_dir="../data/"
+# filename="Ingest_List_3.json"
+# directory="C:/Users/j_sal/git/Ingest"
+# data_dir="../data/"
+
+# Action Sets
+FTP_Action_Set = {"FTP_Download", "FTP_Move", "FTP_Copy", "FTP_Delete"}
+File_Actions = {"Rename", "Unzip"}
+S3_Action_Set = {"S3_Upload", "S3_Move", "S3_Delete"}
 
 
 class Ingest(object):
-    def __init__ (self, stream, myvars:dict):
-        self.source_name_pattern = myvars['Source_Name_Pattern']
-        self.source_dir = myvars['Source_Directory']
-        self.s3_name = myvars['Destination_Name']
-        self.s3_dir = myvars['Destination_Directory']
-        self.action_list = myvars['Actions_List']
-        self.data_dir = "../data/"
+    def __init__(self, stream, ingest_vars: dict, config_vars: dict):
         self.stream = stream
-        
-        
+        self.source_name_pattern = ingest_vars['Source_Name_Pattern']
+        self.source_dir = ingest_vars['Source_Directory']
+        self.s3_name = ingest_vars['Destination_Name']
+        self.s3_dir = ingest_vars['Destination_Directory']
+        self.action_list = ingest_vars['Actions']
+        self.ftp_source = ingest_vars['FTP_Source']
+        self.bucketname = config_vars['Bucketname']
+        self.kms = config_vars['S3_KMS_ARN']
+        self.app_id = config_vars['App_ID']
+        self.app_vault = config_vars['App_Vault']
+        self.zs_password = config_vars['ZS_Password']
+        self.up_password = config_vars['UP_Password']
+        self.ftp_server = config_vars['FTP_DNS']
+        self.data_dir = "../data/"
+
     def rename_files(self):
-        #Add another function to resolve pattern
-        outfile = ""#fetch file pattern to use
+        # Add another function to resolve pattern
+        outfile = self.s3_name  # fetch file pattern to use
         for file in os.listdir(self.data_dir):
             if fnm.fnmatch(file, self.source_name_pattern):
                 os.rename(self.data_dir+file, self.data_dir+outfile)
                 print(file)
         return None
-    
-    
-    def s3_upload(self):
+
+    def file_actions(self):
         return None
 
+    def s3_actions(self):
+        return None
 
-    def ftp_download (self):
-        #create subprocess that will run the lftp process with a Linux script
-        ftp_download.ftp_list_file()
-        
-        #or create python script to ftp files
-        print("ftp_download.sh",
-            f'"{self.source_name_pattern}"',
-            f'"{self.source_dir}"',
-            f'"{self.s3_name}"',
-            f'"{self.s3_dir}"'
-        )
+    def ftp_actions(self):
+        return None
 
-    
-    def print_vars(self):
-        print(self.stream)
-        print(self.source_name_pattern)
-        print(self.source_dir)
-        print(self.s3_name)
-        print(self.s3_dir)
-        print(self.action_list)
-        
-    
     def action_manager(self):
-        self.ftp_download()
-        if "Rename File" in self.action_list:
+        # Ftp Actions
+        if set(self.action_list) & FTP_Action_Set:
+            # redirect to new script
+            self.ftp_actions()
+        # File Actions
+        if set(self.action_list) & File_Actions:
+            # redirect o new script?
             self.rename_files()
-        self.s3_upload()
-        return None
+        # S3 Actions
+        if set(self.action_list) & S3_Action_Set:
+            # redirect to new script
+            self.s3_actions()
 
 
-
-#Not in use
-def work_manager(stream, myvars:dict):
-    print("Work Manager")
-    print("ftp_download", myvars['Source_Name_Pattern'])
-    vals = list (myvars.values())
-    keys = myvars.keys()
-    print("Keys", keys)
-    print("Values", vals)
-
-
-def process_work(myvars):
+def process_work(ingest_vars, config_vars):
     print("--- Process Work ---")
-    for k,v in myvars.items():
-        print("Processing Stream :", k)
-        ingest = Ingest(k,v)
-        #ingest.print_vars()
+    for stream_name, stream_vars in ingest_vars.items():
+        print("Processing Stream :", stream)
+        ingest = Ingest(stream_name, stream_vars, config_vars)
         ingest.action_manager()
 
 
-#Reads filename var declared at top; used for testing
-def read_json ():
-    print("Read JSON")
+# Reads filename var declared at top; used for testing
+def read_json():
+    print("--- Read JSON ---")
     with open(filename) as fn:
         js1 = json.load(fn)
     pprint.pprint(js1)
     return js1
 
 
-#User specifies the json file to be parsed
-def read_args ():
-    print("Read Args")
+# User specifies the json file to be parsed
+def read_args():
+    print("--- Read Args ---")
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--filename', nargs=1, help="JSON Filename", type=argparse.FileType('r'))
+    parser.add_argument("-i", "--ingestfile", nargs=1,
+                        help="Ingestion List Filename", type=argparse.FileType('r'))
+    parser.add_argument("-c", "--configfile", nargs=1,
+                        help="Configuration Filename", type=argparse.FileType('r'))
+    parser.add_argument("-b", "--bucketname", help="S3 Bucketname")
     arguments = parser.parse_args()
-    js = json.load(arguments.filename[0])
-    pprint.pprint(js)
-    return js
+    return arguments
 
-
-def main():
-    print(__name__,"Main")
-    process_work(read_args())
-#    process_work(read_json())
 
 if __name__ == '__main__':
-    main()
+    print(__name__, "Main")
+    arguments = read_args()
+    ingestfile = json.load(arguments.ingestfile[0])
+    configfile = json.load(arguments.configfile[0])
+    pprint.pprint(ingestfile)
+    pprint.pprint(configfile)
+    process_work(ingestfile, configfile)
+#    process_work(read_json())
--- main/ftp_download.py
+++ main/ftp_download.py
@@ -1,34 +1,103 @@
 import argparse
 import os
-import ftplib
+import subprocess
 
-def ftp_list_file():
-    print("Inside FTP Script")
+# lftp  $PROTOCOL://$URL <<- DOWNLOAD
+#     user $USER "$PASS"
+#     cd $REMOTEDIR
+#     mget -E $REGEX
+# DOWNLOAD
+LFTP_INSTALLED = False
+
+
+def run_command(command):
+    return_code = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return return_code
+
+
+def fetch_command(command, *args):
+    if args == ():
+        args = (None, None)
+    command_dict = {
+        "CheckInstall": ["which", "lftp"],
+        "InstallLFTP": ["sudo", "yum", "-y", "install", "lftp"],
+        "FetchPassword" ["sudo", "jsia"]
+        "ListFiles": ["lftp", "-u", args[0], "-p", args[1]],
+        "DownloadFile": ["lftp", "-u", args[0], "-p", args[1]],
+        "DownloadMultiple": ["lftp", "-u", args[0], "-p", args[1]],
+        "DeleteFile": ["lftp", "-u", args[0], "-p", args[1]],
+        "MoveFile": ["lftp", "-u", f"{args[0]},{args[1]}"]
+    }
+    return command_dict[command]
+
+
+def install_ftp():
+    # check if lftp is installed
+    if not LFTP_INSTALLED:
+        command = fetch_command("CheckInstall")
+        print(command)
+        # rc = run_command(fetch_command("CheckInstall"))
+        # if rc.returncode == 0:
+        #     LFTP_INSTALLED = True
+        #     return
+        #  install lftp
+        command = fetch_command("InstallLFTP")
+        print(command)
+        # #rc = run_command(fetch_command("CheckInstall"))
+        # if rc.returncode == 0:
+        #     return rc.stdout
+        # else:
+        #     return rc.stderr
+
+
+def ftp_list_file(ftp_args):
+    print("- Ftp List -")
+    command = fetch_command("ListFiles")
+    print(command)
     return None
 
 
 def ftp_download_file():
+    print("- Ftp Download -")
+    command = fetch_command("DownloadFile")
+    print(command)
     return None
 
 
 def ftp_download_multiple():
+    print("- Ftp Download Multi -")
+    command = fetch_command("DownloadMultiple")
+    print(command)
     return None
 
 
-def ftp_list_delete():
+def ftp_delete():
+    print("- FTP_Delete -")
+    command = fetch_command("DeleteFile")
+    print(command)
     return None
-    
 
-def parse_args():
+
+def move_file():
+    print("- FTP_Move -")
+    command = fetch_command("MoveFile")
+    print(command)
+    return None
+
+
+def read_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--filepattern", help="file name or pattern")
     parser.add_argument("-d", "--directory", help="file source/directory")
     parser.add_argument("-u", "--user", help="application or user ID")
     parser.add_argument("-p", "--password", help="password")
     parser.add_argument("-s", "--server", help="ftp server")
-    
     arguments = parser.parse_args()
+    return arguments
 
 
 if __name__ == "__main__":
-    ftp_list_file()
+    main()
+    install_ftp()
+    ftp_args = read_args()
+    files_to_download = ftp_list_file(ftp_args)
